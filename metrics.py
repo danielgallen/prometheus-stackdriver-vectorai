0a5ba24056cc2d0ec7222a6c95be70c829469235
tubbytoad <1allendan2@gmail.com>
2020-02-17 13:35:59 +0000

    Updating Gauge and Histogram so service can be specified

--- metrics.py
+++ metrics.py
@@ -2,55 +2,45 @@ import prometheus_client as prom
 import time
 from google.cloud import monitoring_v3
 
-class PromCounter:
-    """Prometheus counter"""
-    def __init__(self, name, desc, labels):
-        self.c = Counter(name, desc, labels)
+class Gauge:
+    """Gauge"""
+    def __init__(self, name, desc, service):
+        self.service = service
+        if self.service == "prometheus":
+            self.g = prom.Gauge(name, desc)
+        else:
+            metrics_descriptor = {
+                    "type": custom_type,
+                    "metricKind": "GAUGE",
+                    "valueType": "INT64",
+                    "unit": "items",
+                    "description": desc,
+                    "displayName": name
+            }
+            self.g = client.projects().metricDescriptors().create(
+                     name=name, body=metrics_descriptor).execute() 
     def inc(self, v=1):
-        self.c.inc(v)
-
-class PromGauge:
-    """Prometheus gauge"""
-    def __init__(self, name, desc):
-        self.g = prom.Gauge(name, desc)
-    def inc(self, v=1):
-        return self.g.inc(v)
+        self.g.inc(v)
     def dec(self, v=1):
-        return self.g.dec(v)
+        self.g.dec(v)
     def set(self, v):
-        return self.g.set(v)
+        self.g.set(v)
+    def get(self):
+        self.g
 
-class PromHistogram:
+class Histogram:
     """Prometheus histogram"""
-    def __init__(self, name, desc, labels):
-        self.h = prom.Histogram(name, desc, labels)
+    def __init__(self, name, desc, service, buckets=None):
+        self.service = service
+        if self.service == "prometheus":
+            if buckets:
+                self.h = prom.Histogram(name, desc, buckets=buckets)
+            else:
+                self.h = prom.Histogram(name, desc)
     def observe(self, v):
         return self.h.observe(v)
 
-#def create_stack_gauge(client, project_id, metric_type):
-
-class StackGauge:
-    def __init__(self, client, project_id, metric_type, metric_kind, name, desc):
-        CUSTOM_METRIC_TYPE = "custom.googleapis.com/{}".format(metric_type)
-        self.client = client
-        metric_descriptor = {
-                "type": metric_type,
-                "labels": [
-                    {
-                        "key": "environment"
-                        "valueType": STRING,
-                        "description": desc
-                    }
-                ],
-                "metricKind": metric_kind,
-                "valueType": "INT64",
-                "unit": "items",
-                "description": desc,
-                "displayName": name
-        }
-        client.projects().metricDescriptors().create(
-                name=project_id, body=metrics_descriptor).execute()
-
+"""
 class StackTimeSeries:
     def __init__(self, project_id, metric_type, v):
         client = monitoring_v3.MetricServiceClient()
@@ -66,7 +56,8 @@ class StackTimeSeries:
         client.create_time_series(project_id, [self.series])
 
     def addPoint(v, t):
-        """ Take value and time point and add to TimeSeries """
+        #Take value and time point and add to TimeSeries 
         point = self.series.points.add()
         point.value.double_value = v
         point.interval.end_time.seconds = int(t)
+"""
