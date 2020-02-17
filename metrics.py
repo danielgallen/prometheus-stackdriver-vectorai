83f845bfbdf45115dd0a3c8dd0f38cd9c36f825e
tubbytoad <1allendan2@gmail.com>
2020-02-17 17:42:41 +0000

    Updates

--- metrics.py
+++ metrics.py
@@ -5,36 +5,50 @@ from google.cloud import monitoring_v3
 class Gauge:
     """Gauge"""
     def __init__(self, name, desc, service, metric_type=None):
+        """ Create the gauge metric """
         self.service = service
         if self.service == "prometheus":
             self.g = prom.Gauge(name, desc)
+            print('Prometheus Created {}.'.format(name))
         else:
+            # STACKDRIVER
             self.client = monitoring_v3.MetricServiceClient()
             self.project_name = self.client.project_path(name)
             descriptor = monitoring_v3.types.MetricDescriptor()
             descriptor.type = 'custom.googleapis.com/{}'.format(metric_type)
+            # Gauge
             descriptor.metric_kind = (
                     monitoring_v3.enums.MetricDescriptor.MetricKind.GAUGE)
+            # Double type (Will add switch for types later)
             descriptor.value_type = (
                     monitoring_v3.enums.MetricDescriptor.ValueType.DOUBLE)
             descriptor.description = desc
+            
+            # Create the metric descriptor and print a success message
             descriptor = self.client.create_metric_descriptor(self.project_name, descriptor)
-            print('Created {}.'.format(descriptor.name))
+            print('StackDriver Created {}.'.format(descriptor.name))
+
     def inc(self, v=1):
         self.g.inc(v)
+
     def dec(self, v=1):
         self.g.dec(v)
+
     def set(self, v):
         if self.service == "prometheus":
             self.g.set(v)
         else:
+            # STACKDRIVER
+            # Can only write maximum of one point call per minute for an individual time series
             point = self.g.points.add()
             point.value.double_value = v
             now = time.time()
             point.interval.end_time.seconds = int(now)
             point.interval.end_time.nanos = int(
                     (now - point.interval.end_time.seconds) * 10 ** 9)
+            
             self.client.create_time_series(project_name, [self.g])
+
     def delete(self):
         """ Delete the metric """
         if self.service == "prometheus":
@@ -42,7 +56,7 @@ class Gauge:
             self.client.delete_metric_descriptor(self.name)
 
 class Histogram:
-    """Prometheus histogram"""
+    """Histogram"""
     def __init__(self, name, desc, service, buckets=None, client=None, valueType=None):
         self.service = service
         self.name = name
